285d0e51ecd09141dcd999889ccd627a4c7c13c7
Wiliam Souza <wiliamsouza83@gmail.com>
2015-10-15 23:56:07 -0300

    Adds more mock tests

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -1,9 +1,21 @@
 # coding: utf-8
+import json
+from urllib.parse import urlsplit, splitport
+
+import pytest
+
+import aiohttp
 
 from echod.mock import Mock
 
+request_headers = {
+    'content_type': 'application/json',
+    'accept': 'application/json',
+}
 
-def test_mock_client():
+
+@pytest.mark.asyncio
+def test_mock_client(api_server):
     expectation = {
         'method': 'POST',
         'path': '/v1/users/',
@@ -16,9 +28,46 @@ def test_mock_client():
                      'headers': {'content_type': 'application/json'},
                      'status_code': 201}
     }
-    with Mock(expectation) as client:
+
+    netloc = urlsplit(api_server).netloc
+    host, port = splitport(netloc)
+
+    with Mock(expectation, host, port) as client:
         health = client.health()
         response = client.response()
 
         assert health.status_code == 200
         assert response.status_code == 201
+
+        data = json.dumps(expectation['request']['body'])
+        response = yield from aiohttp.request('POST', client.mock_url,
+                                              data=data,
+                                              headers=request_headers)
+        assert response.status == 201
+
+
+@pytest.mark.asyncio
+def test_mock_client_without_request(api_server):
+    expectation = {
+        'method': 'POST',
+        'path': '/v1/users/',
+        'response': {'body': {'email': 'john@doe.com', 'name': 'John Doe'},
+                     'headers': {'content_type': 'application/json'},
+                     'status_code': 201}
+    }
+
+    netloc = urlsplit(api_server).netloc
+    host, port = splitport(netloc)
+
+    with Mock(expectation, host, port) as client:
+        health = client.health()
+        response = client.response()
+
+        assert health.status_code == 200
+        assert response.status_code == 201
+
+        data = json.dumps('')
+        response = yield from aiohttp.request('POST', client.mock_url,
+                                              data=data,
+                                              headers=request_headers)
+        assert response.status == 201
